843ad0c352bdb26329620b61c08b496f66b677f6
Matjaž <dev@matjaz.it>
2016-03-13 13:28:00 +0100

    Rename table passwd to users, add integer id to it

--- mysql_setup.sql
+++ mysql_setup.sql
@@ -7,18 +7,18 @@ GRANT SELECT ON `squid`.* to `squid`@`localhost` IDENTIFIED BY 'squidpwtomysql';
 
 USE `squid`;
 
-CREATE TABLE `passwd` (
-      `user`     VARCHAR(32) NOT NULL DEFAULT ''
+CREATE TABLE `users` (
+      `id`       INT(16)     NOT NULL AUTO_INCREMENT PRIMARY KEY
+    , `user`     VARCHAR(32) NOT NULL UNIQUE
     , `password` VARCHAR(35) NOT NULL DEFAULT ''
     , `enabled`  TINYINT(1)  NOT NULL DEFAULT '1'
     , `fullname` VARCHAR(60)          DEFAULT NULL
     , `comment`  VARCHAR(60)          DEFAULT NULL
-    
-    PRIMARY KEY (`user`)
     );
 
-INSERT INTO `passwd` VALUES
-    ('testuser','test',1,'Test User','for testing purpose');
+INSERT INTO `users` (`user`, `password`, `fullname`, `comment`) VALUES
+      ('testuser', 'test', 'Test user', 'for testing purpouse')
+    ;
 
 CREATE TABLE `blacklist` (
       `id`      INT(16)      NOT NULL AUTO_INCREMENT PRIMARY KEY
