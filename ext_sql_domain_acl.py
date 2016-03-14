eec76ee5a98b26e6b1cc55f186959e55720db455
Matjaž <dev@matjaz.it>
2016-03-15 00:03:10 +0100

    Refactor command line parameter names in squid configuration
    Also change the decription of the helper script

--- ext_sql_domain_acl.py
+++ ext_sql_domain_acl.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Python3 Helper script for Squid3 to check if an authenticated user is
-# allowed to access a certain domain or not
+# Squid3 external ACL helper script to check if an authenticated user is allowed
+# to access a certain domain or not
 
 import psycopg2
 import psycopg2.extensions
