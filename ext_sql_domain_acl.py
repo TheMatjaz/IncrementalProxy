97a99183990182e054825a60912a9cbca9ebffdc
Matjaž <dev@matjaz.it>
2016-03-14 23:59:40 +0100

    Add parsing of command line parameters with default values
    The default values are set in the command line parameters parsing
    rather than inside objects. The prepared statement is passed to the
    object during construction.

--- ext_sql_domain_acl.py
+++ ext_sql_domain_acl.py
@@ -8,15 +8,17 @@ import psycopg2
 import psycopg2.extensions
 from urllib.parse import urlparse
 from sys import stdin
+import argparse
 
 class DomainAccessControllerOnPostgreSql(object):
-    def __init__(self, persist_connection = True, db_host = "localhost", db_name = "squid", db_user = "squid", db_passwd = "squidpostgresqlpw"):
+    def __init__(self, persist_connection, db_host, db_name, db_user, db_passwd, statement):
         self.persist_connection = persist_connection;
         self.connection = None
-        self.db_host = "localhost"
-        self.db_name = "squid"
-        self.db_user = "squid"
-        self.db_passwd = "squidpostgresqlpw"
+        self.db_host = db_host
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_passwd = db_passwd
+        self.prepared_statement = statement
         self.prepared_cursor = None
         self.error_string = None
 
@@ -33,8 +35,7 @@ class DomainAccessControllerOnPostgreSql(object):
         else:
             return True
 
-
-    def prepare_statement_if_not_already(self, statement):
+    def prepare_statement_if_not_already(self):
         if self.prepared_cursor is not None:
             try:
                 self.prepared_cursor = self.connection.cursor(cursor_factory = PreparingCursor)
@@ -42,7 +43,7 @@ class DomainAccessControllerOnPostgreSql(object):
                 self.error_string = "ERR Unable to create cursor for prepared statement"
                 return False
             try:
-                self.prepared_cursor.prepare(statement)
+                self.prepared_cursor.prepare(self.prepared_statement)
             except:
                 self.error_string = "ERR Unable to prepare statement"
                 return False
@@ -50,7 +51,6 @@ class DomainAccessControllerOnPostgreSql(object):
         else:
             return True
     
-
     def close_db_connection_if_open(self):
         if self.connection is not None:
             try:
@@ -84,6 +84,7 @@ class DomainAccessControllerOnPostgreSql(object):
             # self.error_string = "OK" # Tags?
             return True
 
+
 def extract_domain_from_url(url):
     # Thanks to: http://stackoverflow.com/a/21564306/5292928
     parse_result = urlparse(url) # From urllib.parse
@@ -101,13 +102,9 @@ def extract_domain_and_username_from_line(line):
     url = url.strip('%')
     username = url.strip('%')
     return extract_domain_from_url(url), username
-        
-def main():
-    db_table = "incrementalproxy.vw_domains_per_user"
-    db_username_column = "username"
-    db_domain_column = "domain"
-    prepared_statement = "SELECT TRUE FROM {:s} WHERE {:s} = %s AND %s LIKE {:s}".format(db_table, db_username_column, db_domain_column)
-    controller = DomainAccessControllerOnPostgreSql()
+
+def cycle_over_stdin_lines(controller):
+    collect() # Garbage collection to reduce memory before starting
     for line in stdin:
         if controller.open_db_connection_if_closed() == False:
             print(controller.error_string)
@@ -125,6 +122,43 @@ def main():
                 print(controller.error_string)
                 continue
 
-    
+def parse_command_line_arguments():
+    this_program_description = """\
+Squid3 external ACL helper script to check if an authenticated user
+is allowed to access a certain domain or not"""
+    parser = argparse.ArgumentParser(description = this_program_description)
+    parser.add_argument("--db-user",
+                        default = "squid",
+                        help = "User for database access")
+    parser.add_argument("--db-password",
+                        default = "squidpostgresqlpw",
+                        help = "Clear text password for database access")
+    parser.add_argument("--persist-connection",
+                        default = True,
+                        help = "Keep the database connection open between queries")
+    parser.add_argument("--db-host",
+                        default = "localhost",
+                        help = "Host of the database as DNS name or IP address")
+    parser.add_argument("--db-name",
+                        default = "squid",
+                        help = "Name of the database to connect to")
+    parser.add_argument("--db-table",
+                        default = "incrementalproxy.vw_domains_per_user",
+                        help = "Name of the table to select usernames and domains from")
+    parser.add_argument("--col-domain",
+                        default = "domain",
+                        help = "Name of the column with the domains")
+    parser.add_argument("--col-username",
+                        default = "username",
+                        help = "Name of the column with the usernames")
+    arguments_dict = parser.parse_args()
+    return arguments_dict
+
+def main():
+    args = parse_command_line_arguments()
+    prepared_statement = "SELECT TRUE FROM {:s} WHERE {:s} = %s AND %s LIKE {:s}".format(args.db_table, args.db_username_column, args.db_domain_column)
+    controller = DomainAccessControllerOnPostgreSql(args.persist_connection, args.db_host, args.db_name, args.db_user, args.db_passwd, prepared_statement)
+    cycle_over_stdin_lines(controller)
+
 if __name__ == "__main__":
     main()
