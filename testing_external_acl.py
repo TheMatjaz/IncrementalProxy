08034ae37e65affe2d4db868c30485b82c8c9b2e
Matjaž <dev@matjaz.it>
2016-03-14 23:28:56 +0100

    Add testing script to log what Squid passes to the helper
    This will help better understand how the interaction of the Squid proxy
    works with the helper script, how much is called and what could be
    fixed whatsoever.

---
+++ testing_external_acl.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Testing script to understand how and what Squid passes to an external_acl
+# helper script
+
+from sys import stdin
+
+def extract_domain_from_url(url):
+    # Thanks to: http://stackoverflow.com/a/21564306/5292928
+    parse_result = urlparse(url) # From urllib.parse
+    if parse_result.netloc != '':
+        return parse_result.netloc
+    else:
+        # Probably happens if the url has no protocol
+        # example: "facebook.com/messages/something.html"
+        return parse_result.netloc.split('/', 1)[0]
+
+def extract_domain_and_username_from_line(line):
+    # The line is formatted as "URL username"
+    # Example: "https://www.facebook.com/index.html?var=2 johndoe"
+    url, username = line.strip().split(' ', 1)
+    url = url.strip('%')
+    username = url.strip('%')
+    return extract_domain_from_url(url), username
+
+
+logfile = open("/tmp/squidhelper.log", 'a')
+for line in stdin:
+    logfile.write(line)
+    domain, username = extract_domain_and_username_from_line(line)
+    logfile.write(domain, username)
+    print("OK")
+
+logfile.close()
