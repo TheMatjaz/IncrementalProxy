78f8d0f4039e860f90c7ad3b55a2c42cc48a7a4a
Matjaž <dev@matjaz.it>
2016-03-15 12:56:16 +0100

    Refactor fixes to testing script, add missing import

--- testing_external_acl.py
+++ testing_external_acl.py
@@ -5,6 +5,7 @@
 # helper script
 
 from sys import stdin
+from urllib.parse import urlparse
 
 def extract_domain_from_url(url):
     # Thanks to: http://stackoverflow.com/a/21564306/5292928
@@ -14,14 +15,14 @@ def extract_domain_from_url(url):
     else:
         # Probably happens if the url has no protocol
         # example: "facebook.com/messages/something.html"
-        return parse_result.netloc.split('/', 1)[0]
+        return parse_result.path.split('/', 1)[0]
 
 def extract_domain_and_username_from_line(line):
     # The line is formatted as "URL username"
     # Example: "https://www.facebook.com/index.html?var=2 johndoe"
     url, username = line.strip().split(' ', 1)
     url = url.strip('%')
-    username = url.strip('%')
+    username = username.strip('%')
     return extract_domain_from_url(url), username
 
 
@@ -29,7 +30,7 @@ logfile = open("/tmp/squidhelper.log", 'a')
 for line in stdin:
     logfile.write(line)
     domain, username = extract_domain_and_username_from_line(line)
-    logfile.write(domain, username)
+    logfile.write(domain + " " + username)
     print("OK")
 
 logfile.close()
