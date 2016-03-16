ab612dfc9d792a4c6bece9e62ff68b4c088a3aae
Matjaž <dev@matjaz.it>
2016-03-16 15:32:01 +0100

    Add tw more users and few domains

--- 03_postgresql_setup.sql
+++ 03_postgresql_setup.sql
@@ -1,19 +1,24 @@
 INSERT INTO incrementalproxy.users (username, password, fullname, comment) VALUES
     ('testuser', 'test', 'Mr. Test User', 'For testing purpouse')
   , ('testuser2', 'test', 'Mr. Test User 2', NULL)
+  , ('gustin', 'pwgustin', 'Matjaž Guštin', 'Admin')
+  , ('davanzo', 'UnaPasswordACaso', 'Giorgio Davanzo', 'Professore')
     ;
 
 INSERT INTO incrementalproxy.domains (domain) VALUES
-    ('facebook.com')
+    ('matjaz.it')
+  , ('proxy.matjaz.it')
+  , ('facebook.com')
   , ('twitter.com')
   , ('pintrest.com')
   , ('youtube.com')
   , ('vimeo.com')
     ;
-
+    
 INSERT INTO incrementalproxy.domains_per_user (fk_id_user, fk_id_domain) VALUES
     ((SELECT id FROM users WHERE username = 'gustin'), (SELECT id FROM domains WHERE domain = 'facebook.com'))
   , ((SELECT id FROM users WHERE username = 'gustin'), (SELECT id FROM domains WHERE domain = 'twitter.com'))
   , ((SELECT id FROM users WHERE username = 'davanzo'), (SELECT id FROM domains WHERE domain = 'facebook.com'))
   , ((SELECT id FROM users WHERE username = 'davanzo'), (SELECT id FROM domains WHERE domain = 'youtube.com'))
     ;
+
