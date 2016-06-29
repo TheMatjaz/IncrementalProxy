4eb79af3c5e025b2b8abd5b812d148b9a40ebe21
Matjaž <dev@matjaz.it>
2016-06-29 11:20:09 +0000

    Reduce squid helper log level, fix html and db query column name

--- webpages/insertinlimbo.php
+++ webpages/insertinlimbo.php
@@ -6,6 +6,7 @@ $username = pg_escape_string(trim($_POST['username']));
 $password = pg_escape_string(trim($_POST['password']));
 $domain = pg_escape_string(trim($_POST['domain']));
 $motivation = pg_escape_string(trim($_POST['motivation']));
+$url = pg_escape_string(trim($_POST['url']));
 $authenticated = FALSE;
 $response = "";
 
@@ -23,10 +24,10 @@ if (!$authenticated) {
     $response = "</head><body><h2>Login failed</h2><p>Wrong username or password. Go back and try again.</p>";
 } else {
     // Insert domain into limbo in the database
-    $statement = $db_handle->prepare("INSERT INTO incrementalproxy.vw_domains_per_user (username, domain, status, motivation) VALUES (:username, :domain, 'limbo', :motivation);");
+    $statement = $db_handle->prepare("INSERT INTO incrementalproxy.vw_domains_per_user (username, domain, status, reason) VALUES (:username, :domain, 'limbo', :motivation);");
     $success = $statement->execute(array('username' => $username, 'domain' => $domain, 'motivation' => $motivation));
     if ($success) {
-        $response = "<meta http-equiv=\"refresh\" content=\"1;URL='" . $url . " /></head><body><h2>Domain in limbo</h2><p>The <a href=\"" . $url . "\">requested domain</a> is now in limbo status. You may go on it but will be moderated by the proxy administrator for future usage.</p>";
+        $response = "<meta http-equiv=\"refresh\" content=\"2;URL='" . $url . "'\" /></head><body><h2>Domain in limbo</h2><p>The <a href=\"" . $url . "\">requested domain</a> is now in limbo status. You may go on it but will be moderated by the proxy administrator for future usage.</p>";
     } else {
         $response = "</head><body><h2>Database error</h2><p>An error occurred while inserting your request in the database.</p>";
     }
